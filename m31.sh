9070e6fd61589abe826c3c3979804aa2b9701c45
Revanth Strakz <dr.revanthstrakz@gmail.com>
2020-09-03 15:47:19 +0530

    Update m31.sh

--- m31.sh
+++ m31.sh
@@ -7,20 +7,17 @@ export SUBARCH=arm64
 export ANDROID_MAJOR_VERSION=q
 KBUILD_COMPILER_STRING=$("$TC_DIR"/bin/clang --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g' -e 's/[[:space:]]*$//')
 PATH=$TC_DIR/bin/:$PATH
-MAKE+=(
-			CROSS_COMPILE=aarch64-linux-gnu- \
-			CROSS_COMPILE_ARM32=arm-linux-gnueabi- \
-			CC=clang \
-			AR=llvm-ar \
-			OBJDUMP=llvm-objdump \
-			STRIP=llvm-strip
-		)
+#MAKE+=(
+#			CROSS_COMPILE=aarch64-linux-gnu- \
+#			CROSS_COMPILE_ARM32=arm-linux-gnueabi- \
+#			CC=clang \
+#			AR=llvm-ar \
+#			OBJDUMP=llvm-objdump \
+#			STRIP=llvm-strip
+#		)
 DEFCONFIG=exynos9610-m31nsxx_defconfig
-make O=out $DEFCONFIG
-make -j"$PROCS" O=out \
-		NM=llvm-nm \
-		OBJCOPY=llvm-objcopy \
-		LD=ld.lld "${MAKE[@]}" 2>&1 | tee error.log
+make $DEFCONFIG
+make -j"$PROCS"
 
 
 	
